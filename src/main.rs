aac56d2188cde9f3261401e6d72796c7b9c7671a
bee1oss <46090840+bee1oss@users.noreply.github.com>
2023-07-21 13:38:21 +0300

    end

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,12 @@ trait PrintLOL {
     }
 }
 
+trait PrintHello {
+    fn print_hello(&self) {
+        println!("Hello");
+    }
+}
+
 struct Fgh {
     data: String,
 }
@@ -16,16 +22,32 @@ impl PrintLOL for Fgh {}
 
 impl PrintLOL for DDD {}
 
+impl PrintHello for Fgh {}
+
+impl PrintHello for DDD {}
+
 fn main() {
     let data = Fgh {
         data: "Data ?".to_string(),
     };
-    get_objects(data);
+    //get_objects(data);
+
+    let data_return = return_object();
+    data_return.print_hello();
 }
 
 fn get_object(obj: impl PrintLOL) {
     obj.print_lol();
 }
-fn get_objects<T:PrintLOL>(obj:T) {
+
+fn get_objects<T>(obj: T)
+    where T: PrintLOL + PrintHello {//ukazat ogranicheniya tipaja s pomoshi where
     obj.print_lol();
+    obj.print_hello();
+}
+
+fn return_object()->impl PrintHello{
+    Fgh{
+        data: "Hello from Fgh".to_string(),
+    }
 }
