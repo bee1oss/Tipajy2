9866edeb258349a9c12ef519907b6167037af83b
bee1oss <46090840+bee1oss@users.noreply.github.com>
2023-07-21 13:23:04 +0300

    func
    how to use func with impl

--- src/main.rs
+++ src/main.rs
@@ -8,11 +8,21 @@ struct Fgh {
     data: String,
 }
 
+struct DDD {
+    num: i32,
+}
+
 impl PrintLOL for Fgh {}
+
+impl PrintLOL for DDD {}
 
 fn main() {
     let data = Fgh {
         data: "Data ?".to_string(),
     };
-    data.print_lol();
+    get_object(data);
+}
+
+fn get_object(obj: impl PrintLOL) {
+    obj.print_lol();
 }
