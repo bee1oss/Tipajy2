8d5c10163e5b6e8036e99973a3159d5aa63a6025
bee1oss <46090840+bee1oss@users.noreply.github.com>
2023-07-21 13:30:35 +0300

    examples
    adding some examples for how to use impl

--- src/main.rs
+++ src/main.rs
@@ -20,9 +20,12 @@ fn main() {
     let data = Fgh {
         data: "Data ?".to_string(),
     };
-    get_object(data);
+    get_objects(data);
 }
 
 fn get_object(obj: impl PrintLOL) {
+    obj.print_lol();
+}
+fn get_objects<T:PrintLOL>(obj:T) {
     obj.print_lol();
 }
